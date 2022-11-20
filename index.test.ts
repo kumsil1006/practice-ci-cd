8b8b92f3fd7a5cba0fd77e746634e69ed725b580
kumsil1006 <39304306+kumsil1006@users.noreply.github.com>
2022-11-20 20:20:47 +0900

    test code 수정

--- index.test.ts
+++ index.test.ts
@@ -3,6 +3,7 @@ import fizzBuzz from './index';
 test("Fizz", () => {
   expect(fizzBuzz(3)).toBe("Fizz");
   expect(fizzBuzz(9)).toBe("Fizz");
+  expect(fizzBuzz(51)).toBe("Fizz");
 });
 
 test("Buzz", () => {
@@ -19,5 +20,4 @@ test("number", () => {
   expect(fizzBuzz(1)).toBe(1);
   expect(fizzBuzz(2)).toBe(2);
   expect(fizzBuzz(49)).toBe(49);
-  expect(fizzBuzz(51)).toBe(51);
 });
